7acfec4147dc1f4ec338ddc144bbe74318194000
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-04-12 20:58:32 +0200

    71 - Scaffolding Our Capstone Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className={"categories-container"}>
+            <div className={"category-container"}>
+                {/* <img /> */}
+                <div className={"category-body-container"}>
+                    <h2>Hats</h2>
+                    <p>Shop Now</p>
+                </div>
+            </div>
+            <div className={"category-container"}>
+                {/* <img /> */}
+                <div className={"category-body-container"}>
+                    <h2>Jackets</h2>
+                    <p>Shop Now</p>
+                </div>
+            </div>
+            <div className={"category-container"}>
+                {/* <img /> */}
+                <div className={"category-body-container"}>
+                    <h2>Sneakers</h2>
+                    <p>Shop Now</p>
+                </div>
+            </div>
+            <div className={"category-container"}>
+                {/* <img /> */}
+                <div className={"category-body-container"}>
+                    <h2>Women</h2>
+                    <p>Shop Now</p>
+                </div>
+            </div>
+            <div className={"category-container"}>
+                {/* <img /> */}
+                <div className={"category-body-container"}>
+                    <h2>Men</h2>
+                    <p>Shop Now</p>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
