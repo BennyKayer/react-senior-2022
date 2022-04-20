62be7ba217ffe7d5e242a4b62af7b4f9f52c6329
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-04-20 19:40:17 +0200

    75 Setting up our categories

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
 const App = () => {
+    const categories = [
+        {
+            id: 1,
+            title: "Hats",
+        },
+        {
+            id: 2,
+            title: "Jackets",
+        },
+        {
+            id: 3,
+            title: "Sneakers",
+        },
+        {
+            id: 4,
+            title: "Women",
+        },
+        {
+            id: 5,
+            title: "Men",
+        },
+    ];
+
     return (
         <div className={"categories-container"}>
-            <div className={"category-container"}>
-                {/* <img /> */}
-                <div className={"category-body-container"}>
-                    <h2>Hats</h2>
-                    <p>Shop Now</p>
-                </div>
-            </div>
-            <div className={"category-container"}>
-                {/* <img /> */}
-                <div className={"category-body-container"}>
-                    <h2>Jackets</h2>
-                    <p>Shop Now</p>
-                </div>
-            </div>
-            <div className={"category-container"}>
-                {/* <img /> */}
-                <div className={"category-body-container"}>
-                    <h2>Sneakers</h2>
-                    <p>Shop Now</p>
-                </div>
-            </div>
-            <div className={"category-container"}>
-                {/* <img /> */}
-                <div className={"category-body-container"}>
-                    <h2>Women</h2>
-                    <p>Shop Now</p>
-                </div>
-            </div>
-            <div className={"category-container"}>
-                {/* <img /> */}
-                <div className={"category-body-container"}>
-                    <h2>Men</h2>
-                    <p>Shop Now</p>
-                </div>
-            </div>
+            {categories.map(({ title, id }) => {
+                return (
+                    <div className={"category-container"} key={id}>
+                        <div className="background-image" />
+                        <div className={"category-body-container"}>
+                            <h2>{title}</h2>
+                            <p>Shop Now</p>
+                        </div>
+                    </div>
+                );
+            })}
         </div>
     );
 };
