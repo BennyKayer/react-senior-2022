d043f4e67c581d45265da2015f841fe59000cfd7
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-04-29 12:34:29 +0200

    83 React Router Outlet, 84 Navigation Bar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Home from "./routes/home/home.component";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+const Navigation = () => {
+    return (
+        <div>
+            <div>
+                <h1>nav</h1>
+            </div>
+            <Outlet />
+        </div>
+    )
+}
 
 const App = () => {
     return (
         <Routes>
-            <Route path={"/"} element={<Home />} />
+            <Route path={"/"} element={<Navigation />}>
+                <Route index={true} element={<Home />} />
+            </Route>
         </Routes>
     );
 };
