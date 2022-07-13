c04c2232d7539ce1373a71bc39c7b64196967cd7
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-07-13 18:22:43 +0200

    #06.96 Sign In With Redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,39 @@
 import {
     signInWithGooglePopup,
     createUserDocumentFromAuth,
+    signInWithGoogleRedirect,
+    auth,
 } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 
 const SignIn = () => {
-    const logGoogleUser = async () => {
+    const logGoogleUserPopup = async () => {
         const response = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(response.user);
     };
 
+    useEffect(() => {
+        const get = async () => {
+            const response = await getRedirectResult(auth);
+            if (response) {
+                const userDocRef = await createUserDocumentFromAuth(
+                    response.user
+                );
+            }
+        };
+        get();
+    }, []);
+
     return (
         <div>
             <h1>Sign in Page</h1>
-            <button onClick={logGoogleUser}>Sign In with Google</button>
+            <button onClick={logGoogleUserPopup}>
+                Sign In with Google Popup
+            </button>
+            <button onClick={signInWithGoogleRedirect}>
+                Sign In with Google Redirect
+            </button>
         </div>
     );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+import {
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+    signInWithRedirect,
+} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
@@ -18,13 +24,16 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
     prompt: "select_account",
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+    signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+    signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
