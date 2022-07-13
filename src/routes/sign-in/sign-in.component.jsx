1e94ad0926c98cca14fb50763addf7a95e11a490
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-07-13 18:24:13 +0200

    Cleanup after 96

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,11 +1,7 @@
 import {
     signInWithGooglePopup,
     createUserDocumentFromAuth,
-    signInWithGoogleRedirect,
-    auth,
 } from "../../utils/firebase/firebase.utils";
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
 
 const SignIn = () => {
     const logGoogleUserPopup = async () => {
@@ -13,26 +9,11 @@ const SignIn = () => {
         const userDocRef = await createUserDocumentFromAuth(response.user);
     };
 
-    useEffect(() => {
-        const get = async () => {
-            const response = await getRedirectResult(auth);
-            if (response) {
-                const userDocRef = await createUserDocumentFromAuth(
-                    response.user
-                );
-            }
-        };
-        get();
-    }, []);
-
     return (
         <div>
             <h1>Sign in Page</h1>
             <button onClick={logGoogleUserPopup}>
                 Sign In with Google Popup
-            </button>
-            <button onClick={signInWithGoogleRedirect}>
-                Sign In with Google Redirect
             </button>
         </div>
     );
