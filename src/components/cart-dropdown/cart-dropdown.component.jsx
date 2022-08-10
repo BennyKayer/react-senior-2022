da67e9920dade2b21b6c37e8d2181c6540f16dfd
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-08-10 21:21:27 +0200

    #11.138 Styled-Component - Cart Dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,8 @@
-import "./cart-dropdown.styles.scss";
+import {
+    CartDropDownContainer,
+    CartItems,
+    EmptyMessage,
+} from "./cart-dropdown.styles.jsx";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 import { useContext } from "react";
@@ -14,14 +18,18 @@ const CartDropdown = () => {
     };
 
     return (
-        <div className={"cart-dropdown-container"}>
-            <div className={"cart-items"}>
-                {cartItems.map((item) => (
-                    <CartItem key={item.id} cartItem={item} />
-                ))}
-            </div>
+        <CartDropDownContainer>
+            <CartItems>
+                {cartItems.length ? (
+                    cartItems.map((item) => (
+                        <CartItem key={item.id} cartItem={item} />
+                    ))
+                ) : (
+                    <EmptyMessage>Your cart is empty.</EmptyMessage>
+                )}
+            </CartItems>
             <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-        </div>
+        </CartDropDownContainer>
     );
 };
 
