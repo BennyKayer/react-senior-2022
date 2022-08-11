fc6ef95ca41b88ea35570db03319054cabb62aa4
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-08-11 10:21:21 +0200

    #11.139 Styled Component Directory Item & Cart Icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,8 @@
-import "./cart-icon.styles.scss";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import {
+    CartIconContainer,
+    ItemCount,
+    ShoppingIcon,
+} from "./cart-icon.styles.jsx";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 
@@ -12,10 +15,10 @@ const CartIcon = () => {
     };
 
     return (
-        <div className={"cart-icon-container"} onClick={onClick}>
-            <ShoppingIcon className={"shopping-icon"} />
-            <span className={"item-count"}>{cartItemsCount}</span>
-        </div>
+        <CartIconContainer onClick={onClick}>
+            <ShoppingIcon />
+            <ItemCount>{cartItemsCount}</ItemCount>
+        </CartIconContainer>
     );
 };
 
