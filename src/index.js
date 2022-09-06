ffe9ddbcee87e6280297ec13711d4eda4218f1f0
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-09-06 10:20:52 +0200

    #14.151 - 152 Redux Setup

--- src/index.js
+++ src/index.js
@@ -7,19 +7,23 @@ import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <UserProvider>
-                <CategoriesProvider>
-                    <CartProvider>
-                        <App />
-                    </CartProvider>
-                </CategoriesProvider>
-            </UserProvider>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <UserProvider>
+                    <CategoriesProvider>
+                        <CartProvider>
+                            <App />
+                        </CartProvider>
+                    </CategoriesProvider>
+                </UserProvider>
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 );
 
---
+++ src/store/user/user.reducer.js
@@ -0,0 +1,22 @@
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const INITIAL_STATE = {
+    currentUser: null,
+};
+
+export const userReducer = (state = INITIAL_STATE, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return { ...state, currentUser: payload };
+        // Necessary to return same object not {...state} as f.e
+        // actions from categories or cart will also trigger this reducer
+        // returning same object will let redux know
+        // OH THIS PART OF ROOT REDUCER DID NOT CHANGED, DON"T RE RENDER
+        default:
+            return state;
+    }
+};
