a60272bb5a47f206b898815de0bf2dbd2f522c3f
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-09-19 17:44:21 +0200

    #15.168 Redux-devtools

--- src/store/store.js
+++ src/store/store.js
@@ -14,9 +14,17 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [logger];
+const middlewares = [process.env.NODE_ENV !== "production" && logger].filter(
+    Boolean
+);
 
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const composeEnhancer =
+    (process.env.NODE_ENV !== "production" &&
+        window &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
 
 export const store = createStore(
     persistedReducer,
