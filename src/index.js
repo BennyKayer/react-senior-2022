c55332294ad13f043fd6aa005fb3c65606c24cb2
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-09-19 17:29:36 +0200

    #15.167 Redux-Persist

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            {/* Inside provider */}
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </PersistGate>
         </Provider>
     </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,27 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
+
+const persistConfig = {
+    key: "root",
+    storage,
+    // Persisting this may cause some weird stuff
+    blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+    persistedReducer,
+    undefined,
+    composedEnhancers
+);
+
+export const persistor = persistStore(store);
