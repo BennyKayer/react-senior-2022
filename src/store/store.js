39f26a990157b541658d9c7b2d797e9d3d7d04b8
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-09-20 13:21:26 +0200

    #16.169 Async Redux - Redux Thunk, intro to start/success/error pattern

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
@@ -14,9 +15,10 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [process.env.NODE_ENV !== "production" && logger].filter(
-    Boolean
-);
+const middlewares = [
+    process.env.NODE_ENV !== "production" && logger,
+    thunk,
+].filter(Boolean);
 
 const composeEnhancer =
     (process.env.NODE_ENV !== "production" &&
