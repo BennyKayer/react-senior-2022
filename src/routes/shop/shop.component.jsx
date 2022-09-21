dbcdef35af21fda268d8e7bc9ebd4f6fcdf96377
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-09-21 21:43:22 +0200

    #16.170 Redux Thunk pt.2

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,19 +3,14 @@ import "./shot.styles.scss";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import { useEffect } from "react";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategories } from "../../store/categories/category.action";
+import { fetchCategoriesAsync } from "../../store/categories/category.action";
 import { useDispatch } from "react-redux";
 
 const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const getCategoriesMap = async () => {
-            const categoriesArray = await getCategoriesAndDocuments();
-            dispatch(setCategories(categoriesArray));
-        };
-        getCategoriesMap();
+        dispatch(fetchCategoriesAsync());
     }, [dispatch]);
 
     return (
