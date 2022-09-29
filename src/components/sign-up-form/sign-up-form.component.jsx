cd7d27648ba7733bde9067b03f83ad39112e198e
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-09-29 09:47:43 +0200

    #17.179 Redux-Saga Sign up Sagas

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
-import {
-    createAuthUserWithEmailAndPassword,
-    createUserDocumentFromAuth,
-} from "../../utils/firebase/firebase.utils";
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import FormInput from "../form-input/form-input.component";
 import "./sign-up-form.styles.scss";
+import { useDispatch } from "react-redux";
+import { signUpStart } from "../../store/user/user.action";
 
 const defaultFormFields = {
     displayName: "",
@@ -17,6 +15,7 @@ const defaultFormFields = {
 const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
+    const dispatch = useDispatch();
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -32,13 +31,7 @@ const SignUpForm = () => {
         }
 
         try {
-            const { user } = await createAuthUserWithEmailAndPassword(
-                email,
-                password
-            );
-
-            // TODO: Firebase listener problematic with display name
-            await createUserDocumentFromAuth(user, { displayName });
+            dispatch(signUpStart(email, password, displayName));
             setFormFields(defaultFormFields);
         } catch (err) {
             if (err.code === "auth/email-already-in-use") {
--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -1,11 +1,17 @@
 import { all, call, put, takeLatest } from "redux-saga/effects";
 import { USER_ACTION_TYPES } from "./user.action-types";
-import { signInFailed, signInSuccess } from "./user.action";
+import {
+    signInFailed,
+    signInSuccess,
+    signUpFailed,
+    signUpSuccess,
+} from "./user.action";
 import {
     getCurrentUser,
     createUserDocumentFromAuth,
     signInWithGooglePopup,
     signInAuthUserWithEmailAndPassword,
+    createAuthUserWithEmailAndPassword,
 } from "../../utils/firebase/firebase.utils";
 
 export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
@@ -55,6 +61,23 @@ export function* signInWithEmail({ payload: { email, password } }) {
     }
 }
 
+export function* signUp({ payload: { email, password, displayName } }) {
+    try {
+        const { user } = yield call(
+            createAuthUserWithEmailAndPassword,
+            email,
+            password
+        );
+        yield put(signUpSuccess(user, { displayName }));
+    } catch (error) {
+        yield put(signUpFailed(error));
+    }
+}
+
+export function* signInAfterSignUp({ payload: { user, additionalDetails } }) {
+    yield call(getSnapshotFromUserAuth, user, additionalDetails);
+}
+
 export function* onGoogleSignInStart() {
     yield takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -67,10 +90,20 @@ export function* onEmailSignInStart() {
     yield takeLatest(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, signInWithEmail);
 }
 
+export function* onSignUpStart() {
+    yield takeLatest(USER_ACTION_TYPES.SIGN_UP_START, signUp);
+}
+
+export function* onSignUpSuccess() {
+    yield takeLatest(USER_ACTION_TYPES.SIGN_UP_SUCCESS, signInAfterSignUp);
+}
+
 export function* userSagas() {
     yield all([
         call(onCheckUserSession),
         call(onGoogleSignInStart),
         call(onEmailSignInStart),
+        call(onSignUpStart),
+        call(onSignUpSuccess),
     ]);
 }
