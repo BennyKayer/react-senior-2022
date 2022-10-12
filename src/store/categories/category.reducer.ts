99ebde747745637ded949607bcb8b9a8f81df857
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-10-12 19:01:13 +0200

    #20.200 Typing Category Selectors

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,7 +1,13 @@
-import { CATEGORIES_ACTION_TYPES } from "./category.types";
+import { CATEGORIES_ACTION_TYPES, Category } from "./category.types";
 import { CategoryAction } from "./category.action";
 
-export const CATEGORIES_INITIAL_STATE = {
+export type CategoriesState = {
+    readonly categories: Category[];
+    readonly isLoading: boolean;
+    readonly error: Error | null;
+};
+
+export const CATEGORIES_INITIAL_STATE: CategoriesState = {
     categories: [],
     isLoading: false,
     error: null,
@@ -11,21 +17,19 @@ export const categoriesReducer = (
     state = CATEGORIES_INITIAL_STATE,
     action = {} as CategoryAction
 ) => {
-    const { type, payload } = action;
-
-    switch (type) {
+    switch (action.type) {
         case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
             return { ...state, isLoading: true };
         case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
             return {
                 ...state,
-                categories: payload,
+                categories: action.payload,
                 isLoading: false,
             };
         case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
             return {
                 ...state,
-                error: payload,
+                error: action.payload,
                 isLoading: false,
             };
         default:
--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -1,6 +1,9 @@
 import { createSelector } from "reselect";
 
-const selectCategoryReducer = (state) => state.categories;
+import { CategoriesState } from "./category.reducer";
+import { CategoryMap } from "./category.types";
+
+const selectCategoryReducer = (state): CategoriesState => state.categories;
 
 export const selectCategories = createSelector(
     [selectCategoryReducer],
@@ -9,8 +12,8 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
     [selectCategories],
-    (categories) => {
-        return categories.reduce((acc, category) => {
+    (categories): CategoryMap => {
+        return categories.reduce<CategoryMap>((acc, category) => {
             const { title, items } = category;
             acc[title.toLowerCase()] = items;
             return acc;
