11f16e57a20437a0fd6c229af81949d2178d7f26
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-10-14 11:54:49 +0200

    #20.204 withMatcher Type

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -6,6 +6,23 @@ type Matchable<AC extends () => AnyAction> = AC & {
     match: (action: AnyAction) => action is ReturnType<AC>;
 };
 
+export function withMatcher<AC extends () => AnyAction & { type: string }>(
+    actionCreator: AC
+): Matchable<AC>;
+export function withMatcher<
+    AC extends (...args: any[]) => AnyAction & { type: string }
+>(actionCreator: AC): Matchable<AC>;
+
+export function withMatcher(actionCreator: Function) {
+    const type = actionCreator().type;
+    return Object.assign(actionCreator, {
+        type,
+        match(action: AnyAction) {
+            return action.type === type;
+        },
+    });
+}
+
 // Why 2 not 1 with optional payload?
 // change of thinking, no point of referencing something that shouldn't be there
 // just creates a hassle with undefined check, which we don't want
