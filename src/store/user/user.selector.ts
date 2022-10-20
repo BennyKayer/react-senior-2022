867bdc922229077dfc6b603757ef65229fb48d6a
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-10-20 16:17:29 +0200

    #22.211 Typing Rootstate

--- src/store/user/user.selector.ts
+++ src/store/user/user.selector.ts
@@ -1,8 +1,9 @@
 import { createSelector } from "reselect";
+import { RootState } from "../store";
 
 import { UserSlice } from "./user.reducer";
 
-export const selectUserReducer = (state: any): UserSlice => state.user;
+export const selectUserReducer = (state: RootState): UserSlice => state.user;
 
 export const selectCurrentUser = createSelector(
     [selectUserReducer],
