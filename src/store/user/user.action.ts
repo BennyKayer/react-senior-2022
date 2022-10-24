27c41bd18510a2ec1f94f18c377f393a9d3500bd
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-10-24 12:33:12 +0200

    #24.215 - 216 Typing User Sagas

--- src/store/user/user.action.ts
+++ src/store/user/user.action.ts
@@ -1,3 +1,4 @@
+import { User } from "firebase/auth";
 import {
     AdditionalInformation,
     UserData,
@@ -44,7 +45,7 @@ export type SignInSuccess = ActionWithPayload<
     UserData
 >;
 export const signInSuccess = withMatcher(
-    (user: SignInSuccess["payload"]): SignInSuccess =>
+    (user: SignInSuccess["payload"] & { id: string }): SignInSuccess =>
         createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user)
 );
 
@@ -76,7 +77,7 @@ export const signUpStart = withMatcher(
 
 export type SignUpSuccess = ActionWithPayload<
     USER_ACTION_TYPES.SIGN_UP_SUCCESS,
-    { user: UserData; additionalDetails: AdditionalInformation }
+    { user: User; additionalDetails: AdditionalInformation }
 >;
 export const signUpSuccess = withMatcher(
     (
